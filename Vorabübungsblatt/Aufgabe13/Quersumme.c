61cee317ca4d37f099866839644aed350ef89e48
raketenlurch <raketenlurch@riseup.net>
2020-07-31 03:12:46 +0200

    Vorabübungsblatt: Add first draft of Aufgabe13

---
+++ Vorabübungsblatt/Aufgabe13/Quersumme.c
@@ -0,0 +1,30 @@
+/* Quersumme.c */
+#include <stdio.h>
+
+int eingabe();
+int quersumme(int a);
+
+int main() {
+  int input = eingabe();
+  int ergebnis = quersumme(ergebnis);
+
+  printf("Die Quersumme von %i ist %i.\n", input, ergebnis);
+  return 0;
+}
+
+int eingabe() {
+  int zahl;
+
+  printf("Bitte geben Sie eine Zahl ein: ");
+  scanf("%i", &zahl);
+  return zahl;  
+}
+
+int quersumme(int a) {
+  int sum = 0;
+  while (a != 0) {
+    sum = sum + (a%10);
+    a = a/10;
+  }
+  return sum;
+}
---
+++ Vorabübungsblatt/Aufgabe13/README.md
@@ -0,0 +1,5 @@
+# Aufgabe 13: Funktionen und Schleifen
+Schreiben Sie ein Programm, das die Quersumme (Summe der Ziffernwerte) einer
+Dezimalzahl $x \in \mathbb{N}$ berechnet und auf dem Bildschrim ausgibt. Dabei
+soll der Benutzer bei Programmstart aufgefordert werden die Zahl $x$ einzugeben.
+Die Berechnung soll in einer eigenen Funktion erfolgen.
