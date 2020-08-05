6ee57bc7cebfe2033901f2e9ab1f4b3ed35df2f2
raketenlurch <raketenlurch@riseup.net>
2020-08-05 02:37:18 +0200

    Tag01: Update Messwerte.c

--- Praktikum/Tag01/uploads/Aufgabe01/Messwerte.c
+++ Praktikum/Tag01/uploads/Aufgabe01/Messwerte.c
@@ -1,26 +1,4 @@
 /* Messwerte.c */
-/*#include <stdio.h>
-#define MAX 12
-
-int main() {
-  int i;
-  double messwerte[MAX];
-
-  printf("Bitte geben Sie 12 Zahlen ein: ");
-  for (i = 0; i < MAX; i++) {
-    printf("%d. Zahl: ", i+1);
-    scanf("%le", &messwerte[i]);
-  }
-
-  printf("Sie haben eingegeben: ");
-  for (i = 0; i < MAX; i++) {
-    printf("%d", i);
-  }
-  printf("\n");
-  return 0;
-}
-*/
-
 #include <stdio.h>
 #define LENGTH 12
 
@@ -38,7 +16,7 @@ int main() {
   // Ausgabe
   printf("Sie haben eingegeben: ");
   for (i=0; i<LENGTH; i++) {
-    printf("%d. Zahl: %f", i, *(messwerte + i));
+    printf("%d. Zahl: %f\n", i, *(messwerte + i));
   }
   return 0;
 }
