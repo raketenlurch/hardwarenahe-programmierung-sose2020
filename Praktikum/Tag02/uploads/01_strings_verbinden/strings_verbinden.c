4cc490bdd3873ef4f6f613e6d8e31008c0d8f5f0
Jan Girlich <vollkorn@cryptobitch.de>
2020-08-09 15:56:13 +0200

    Sprechende Variablennamen

--- Praktikum/Tag02/uploads/01_strings_verbinden/strings_verbinden.c
+++ Praktikum/Tag02/uploads/01_strings_verbinden/strings_verbinden.c
@@ -2,21 +2,21 @@
 #include "strings_verbinden.h"
 
 void strings_verbinden(char *zusammen, char *string1, char *string2) {
-  int i = 0, j = 0;
+  int pos_string1 = 0, pos_zusammen = 0;
 
   // Insert the first string in the new string
-  while (string1[i] != '\0') {
-    zusammen[j] = string1[i];
-    i++;
-    j++;
+  while (string1[pos_string1] != '\0') {
+    zusammen[pos_zusammen] = string1[pos_string1];
+    pos_string1++;
+    pos_zusammen++;
   }
 
   // Insert the second string in the new string
-  int k = 0;
-  while (string2[k] != '\0') {
-    zusammen[j] = string2[k];
-    k++;
-    j++;
+  int pos_string2 = 0;
+  while (string2[pos_string2] != '\0') {
+    zusammen[pos_zusammen] = string2[pos_string2];
+    pos_string2++;
+    pos_zusammen++;
   }
 
   // Print concateneted String
