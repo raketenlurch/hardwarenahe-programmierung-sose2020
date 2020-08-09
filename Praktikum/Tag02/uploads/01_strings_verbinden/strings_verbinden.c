1c17c22ba5a4d203985aafff6d2aaf7dfe3d2a8f
Jan Girlich <vollkorn@cryptobitch.de>
2020-08-09 18:59:55 +0200

    Variablen auf Scope beschränken

--- Praktikum/Tag02/uploads/01_strings_verbinden/strings_verbinden.c
+++ Praktikum/Tag02/uploads/01_strings_verbinden/strings_verbinden.c
@@ -2,18 +2,17 @@
 #include "strings_verbinden.h"
 
 void strings_verbinden(char *zusammen, char *string1, char *string2) {
-  int pos_string1 = 0, pos_zusammen = 0;
+  int pos_zusammen = 0;
 
   // Insert the first string in the new string
-  while (string1[pos_string1] != '\0') {
+  for (int pos_string1 = 0; string1[pos_string1] != '\0';) {
     zusammen[pos_zusammen] = string1[pos_string1];
     pos_string1++;
     pos_zusammen++;
   }
 
   // Insert the second string in the new string
-  int pos_string2 = 0;
-  while (string2[pos_string2] != '\0') {
+  for (int pos_string2 = 0; string2[pos_string2] != '\0';) {
     zusammen[pos_zusammen] = string2[pos_string2];
     pos_string2++;
     pos_zusammen++;
